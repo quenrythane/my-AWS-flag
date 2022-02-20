e50d08b293347703eed26647c01a0b151dbbd8e8
quenrythane <artur.babinski.g@gmail.com>
2022-02-20 12:17:27 +0000

    first commit

--- app.py
+++ app.py
@@ -5,11 +5,19 @@ app=Flask(__name__)
 @app.route('/')
 def index():
     text = open('dane/xd.txt').read()
-    return render_template("index.html", text=text)
+    return render_template("index.html", my_name=text)
 
 @app.route('/xd')
 def xd():
     return render_template("xd.html")
+
+
+@app.route('/password-generator')
+def password_generator():
+    from pliki_python.password_generator import generate_password
+    password = generate_password()
+    return render_template("password-generator.html", new_password=password)
+
 
 if __name__=="__main__":
     app.run()
